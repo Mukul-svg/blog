790fc3ec2361f0bc85d637efb1ebe823729221af
Mukul Raghav <mukulraghav2003@gmail.com>
2022-09-30 00:03:54 +0530

    some changes

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,7 @@ from turtle import title
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -28,6 +29,12 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                             choices=STATUS_CHOICES,
                             default='draft')
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args = [self.publish.year,
+                        self.publish.month,
+                        self.publish.day,
+                        self.slug])
 class Meta:
     ordering = ('-publish',)
 def __str__(self):
